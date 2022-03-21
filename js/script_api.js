f3b4a33462de2d9bea5270197ab3a74be59221e7
Agustin Del Giudice <adelgiudice@stechs.com.ar>
2022-03-21 18:05:30 -0300

    se agregan api de post de T!

--- js/script_api.js
+++ js/script_api.js
@@ -31,3 +31,55 @@ function validateCode(){
 	}
 }
 
+function analitics_post(){
+    //https://www.taringa.net/+taringa_at_night/los-chorizos-invadieron-t_51de97
+    var code = document.getElementById("Code").value;
+    if (code.split('_').length > 1){
+        var id = code.split('_')[(code.split('_').length) - 1]
+    }
+    else{
+        var id = code
+    }
+    var event = httpGetList("https://beta.taringa.net/api/story/"+id+"/voters/down");
+	if (event != null){
+        var Info = httpGetList("https://beta.taringa.net/api/story/"+id+"/voters/down");
+        console.log(Info.items);
+
+        if(Info.items.length > 0){
+            var lista = ''
+            console.log(Info.items.length)
+            //if(List.Invitados.length>0){
+
+            lista += '<li class="list-group-item list-group-item-info">'+'Negativos'+'</li>';
+            for (x of Info.items) {
+              console.log(x.username);
+              if (x != null){
+              lista += '<li class="list-group-item">'+x.username+'</li>';
+              }
+            }
+        }
+        document.getElementById("Lista").innerHTML = lista;
+        var Info = httpGetList("https://beta.taringa.net/api/story/"+id+"/voters/up");
+        console.log(Info.items);
+
+        if(Info.items.length > 0){
+            var lista = ''
+            console.log(Info.items.length)
+            //if(List.Invitados.length>0){
+
+            lista += '<li class="list-group-item list-group-item-info">'+'Positivos'+'</li>';
+            for (x of Info.items) {
+              console.log(x.username);
+              if (x != null){
+              lista += '<li class="list-group-item">'+x.username+'</li>';
+              }
+            }
+        }
+        document.getElementById("Lista-positivos").innerHTML = lista;
+   }
+
+    //inputVal = 'Su codigo es '+id;
+    //alert(inputVal);
+
+}
+
